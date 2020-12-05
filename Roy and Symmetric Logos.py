f8335f2855cf62ad1e81c640d022190b98b3c219
Malware_656 <33384499+Malware656@users.noreply.github.com>
2020-12-05 19:50:42 +0530

    Update Roy and Symmetric Logos.py

--- Roy and Symmetric Logos.py
+++ Roy and Symmetric Logos.py
@@ -14,3 +14,21 @@ for _ in range(t):
         temp = list((input().split()))
         matrix.append(temp)
     print(symmetric(matrix,n))
+    
+# ALTER
+# def symmetric(matrix,n):
+# for m, i in enumerate(range(n), 1):
+#     for k, j in enumerate(range(n), 1):
+#         if int(matrix[i][0][j]) != int(matrix[i][0][-k]):
+#             return 'NO'
+#         if int(matrix[i][0][j]) != int(matrix[-m][0][j]):
+#             return 'NO'
+#     return 'YES'
+# t = int(input())
+# for _ in range(t):
+#     n = int(input())
+#     matrix = []
+#     for i in range(n):
+#         temp = list((input().split()))
+#         matrix.append(temp)
+#     print(symmetric(matrix,n))
